f23f04a1a61074e7f299867ff39f1df10127cbb1
ostrovskyio <ostrovskyoleg2001@gmail.com>
2023-03-02 18:38:30 +0200

    Fix:_script

--- skript.js
+++ skript.js
@@ -5,11 +5,11 @@ firstNumber = prompt('Please, enter first number');
 secondNumber = prompt('Please, enter second number');
 
 alert(`Your results:
-\n
+
 ${firstNumber + '+' + secondNumber + '=' + (Number(firstNumber) + Number(secondNumber))}
-\n
+
 ${firstNumber + '-' + secondNumber + '=' + (firstNumber - secondNumber)}
-\n
+
 ${firstNumber + '*' + secondNumber + '=' + (firstNumber * secondNumber)}
-\n
-${firstNumber + '/' + secondNumber + '=' + (firstNumber / secondNumber)}`);
+
+${firstNumber + '/' + secondNumber + '=' + (firstNumber / secondNumber)}`)
